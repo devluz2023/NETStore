715688b17719d4f403c43bfe16093dfa0f8cc792
binhojulix <fabio.jdluz@gmail.com>
2025-07-21 21:13:05 -0300

    criar=usuario

--- web-store/src/app/components/user-form/user-form.component.ts
+++ web-store/src/app/components/user-form/user-form.component.ts
@@ -1,11 +1,80 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.css'
+  styleUrls: ['./user-form.component.css']
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit {
+  userId: number | null = null;
+  userData: any = {}; // Puedes definir una interfaz específica
+
+  isEditMode: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
+  ) { }
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      if (params['id']) {
+        this.userId = +params['id'];
+        this.loadUser(this.userId);
+        this.isEditMode = true;
+      } else {
+        this.userId = null;
+        this.isEditMode = false;
+        // Datos iniciales vacíos
+        this.userData = {
+          email: '',
+          username: '',
+          password: '',
+          name: { firstname: '', lastname: '' },
+          address: {
+            city: '',
+            street: '',
+            number: null,
+            zipcode: '',
+            geolocation: { lat: '', long: '' }
+          },
+          phone: '',
+          status: 'Active',
+          role: 'Customer'
+        };
+      }
+    });
+  }
+
+  loadUser(id: number) {
+    this.http.get(`http://localhost:80/api/users/${id}`)
+      .subscribe(data => {
+        this.userData = data; // populate form
+      });
+  }
+
+  saveProduct() {
+    if (this.isEditMode && this.userId != null) {
+      // update
+      this.http.put(`http://localhost:80/api/users/${this.userId}`, this.userData)
+        .subscribe(response => {
+          // handle success, maybe navigate away
+        });
+    } else {
+      // create new
+      this.http.post(`http://localhost:80/api/users`, this.userData)
+        .subscribe(response => {
+          // handle success, maybe navigate away
+        });
+    }
+
+    this.router.navigate(['/users']);
+  }
 
 }
